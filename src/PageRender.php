487a57c3f73321d68b564503d2588008b6467222
nece001 <nece001@163.com>
2026-05-23 12:19:15 +0800

    添加请求前拦截

--- src/PageRender.php
+++ src/PageRender.php
@@ -4,4 +4,34 @@ namespace Nece\WebUi\Layui;
 
 use Nece\WebUi\Render\PageRender as RenderPageRender;
 
-class PageRender extends RenderPageRender {}
+class PageRender extends RenderPageRender
+{
+    public function render(): string
+    {
+        $this->addAjaxBeforeIntercept();
+        return parent::render();
+    }
+
+    private function addAjaxBeforeIntercept(): void
+    {
+        $func = $this->component->getConfig('ajax_before_intercept_js_function');
+        var_dump($func);
+        if ($func) {
+            $js = "layui.define(['jquery'], function(exports){
+                var $ = layui.jquery;
+
+                // Layui全局请求拦截 表格/普通ajax全部生效
+                $.ajaxPrefilter(function(options) {
+                    options.headers = options.headers || {};
+
+                    var headers = $func(options.data);
+                    for (var key in headers) {
+                        options.headers[key] = headers[key];
+                    }
+                });
+            });";
+
+            PageRender::addJavaScriptCode($js);
+        }
+    }
+}
